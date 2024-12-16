748820ebd83c31cd7afdc6f5c957a3069729863a
MirellyMaria1 <mmmp1@aluno.ifal.edu.br>
2024-12-16 11:00:04 -0300

    tela renomeada

---
+++ lib/domain/doacoes.dart
@@ -0,0 +1,13 @@
+class Doacoes {
+  String imagem;
+  String? titulo;
+  String? subtitulo;
+  String? valor;
+
+  Doacoes({
+    required this.imagem,
+    this.titulo,
+    this.subtitulo,
+    this.valor,
+  });
+}
