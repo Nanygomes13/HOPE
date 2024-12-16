fbf4fdc15f0dfc00703c2251322242d8e3caa13e
MirellyMaria1 <mmmp1@aluno.ifal.edu.br>
2024-12-16 10:23:55 -0300

    alteraçao main

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:hopee/cadastro.dart';
-import 'package:hopee/login.dart';
-
+import 'package:myapp/pages/TelaInicial.dart';
 
 void main() {
   runApp(
     const MaterialApp(
-      home: Tela_entrar(),
+      home: TelaInicial(),
     ),
   );
 }
