24199020357f0b487c9660c1d92088cc364739aa
Alessandra <ass112@aluno.ifal.edu.br>
2024-12-18 08:39:29 -0300

    Add botão voltar

--- lib/pages/TelaBuscar.dart
+++ lib/pages/TelaBuscar.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:hopee/pages/TelaInicial.dart';
 
 class TelaBuscar extends StatefulWidget {
   const TelaBuscar({super.key});
@@ -17,45 +18,66 @@ class _TelaBuscarState extends State<TelaBuscar> {
           children: [
             Padding(
               padding: const EdgeInsets.only(top: 30, left: 20, right: 20),
-              child: SizedBox(
-                height: 50,
-                child: TextField(
-                  decoration: InputDecoration(
-                    fillColor: const Color.fromARGB(255, 240, 205, 243),
-                    filled: true,
-                    prefixIcon: const Icon(
-                      Icons.search,
+              child: Row(
+                children: [
+                  IconButton(
+                    icon: const Icon(
+                      Icons.arrow_back,
                       color: Colors.deepPurpleAccent,
+                      size: 32,
                     ),
-                    hintText: 'Pesquisar',
-                    contentPadding: const EdgeInsets.symmetric(vertical: 10),
-                    enabledBorder: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(24),
-                      borderSide: const BorderSide(
-                        color: Colors.deepPurpleAccent,
-                      ),
-                    ),
-                    focusedBorder: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(24),
-                      borderSide: const BorderSide(
-                        color: Colors.deepPurpleAccent,
+                    onPressed: () {
+                      Navigator.pop(
+                          context,
+                        MaterialPageRoute(builder: (context) {
+                          return const TelaInicial();
+                        }),
+                      );
+                    },
+                  ),
+                  const SizedBox(width: 10),
+                  Expanded(
+                    child: SizedBox(
+                      height: 50,
+                      child: TextField(
+                        decoration: InputDecoration(
+                          fillColor: const Color.fromARGB(255, 240, 205, 243),
+                          filled: true,
+                          prefixIcon: const Icon(
+                            Icons.search,
+                            color: Colors.deepPurpleAccent,
+                          ),
+                          hintText: 'Pesquisar',
+                          contentPadding:
+                          const EdgeInsets.symmetric(vertical: 10),
+                          enabledBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(24),
+                            borderSide: const BorderSide(
+                              color: Colors.deepPurpleAccent,
+                            ),
+                          ),
+                          focusedBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(24),
+                            borderSide: const BorderSide(
+                              color: Colors.deepPurpleAccent,
+                            ),
+                          ),
+                        ),
                       ),
                     ),
                   ),
-                ),
+                ],
               ),
             ),
-            SizedBox(height: 30),
+            const SizedBox(height: 30),
             Center(
-              child: Expanded(
-                child: Image.asset(
-                  'images/busca.png',
-                  width: 250,
-                  fit: BoxFit.fitWidth,
-                ),
+              child: Image.asset(
+                'images/busca.png',
+                width: 250,
+                fit: BoxFit.fitWidth,
               ),
             ),
-            SizedBox(height: 15),
+            const SizedBox(height: 15),
             const Center(
               child: Text(
                 'Quem você deseja ajudar hoje?',
